c3b2dc8b0916dab902a88628c2f92ece03203ce7
BelisaSalcedo <97406760+BelisaSalcedo@users.noreply.github.com>
2022-01-17 18:31:32 +0100

    Commit principal de pygame

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
-import pygame
-import sys
-
-class runner():
-    pass
+import pygame,sys
 
 class Game():
-    corredores =[]
     
+    runners=[]
+    __startLine=20
+    __finishLine=620
     
     def __init__(self):
+        self.__screen =pygame.display.set_mode((640, 480))
+        self.__background=pygame.image.load('images/background.png')
+        pygame.display.set_caption('Carrera de bichos')
         
-        self.__screen=pygame.display.set_mode((640,480))
-        pygame.display.set_caption ('Carrera de bichos')
-        self.background=pygame.image.load('images/background.png')
-        self.runner= pygame.image.load('images/smallball.png')
-    
-    def competir (self):
-        x=0
-        hayGanador=False
-        while True:
-            #comprobacion de los eventos
+    def competir(self):
+        gameOver=False
+        while not gameOver:
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                    
-            #Refrecar / renderizar la pantalla        
-            self.__screen.blit(self.background,(0,0))
-            self.__screen.blit(self.runner,(x,240) )
+                    gameOver=True
+            self.__screen.blit(self.__background,(0,0))
             pygame.display.flip()
-            
-            x +=3
-            if x >=250:
-                hayGanador=True
         pygame.quit()
         sys.exit()
-
+    
 if __name__=='__main__':
-    pygame.init()
     game=Game()
+    pygame.init()
     game.competir()
+    
